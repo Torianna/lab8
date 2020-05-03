8b0c47a10a4bcd23117dde755180cc83ec9d7895
Klaudia <klaudia.goralska95@gmail.com>
2020-05-03 17:58:36 +0200

    add

--- src/main/java/wizut/tpsi/calculator/CalculatorController.java
+++ src/main/java/wizut/tpsi/calculator/CalculatorController.java
@@ -1,47 +1,62 @@
 package wizut.tpsi.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CalculatorController {
     
+   @Autowired
+    private CalculatorService calculatorService;
     @RequestMapping("/")
     public String home() {
         return "index";
     }
     
-    @RequestMapping("/add")
-    public String addNumbers(Model model, CalculatorForm form) {
-        int result = form.getX() + form.getY();
+    @PostMapping("/add")
+    public String addNumbers(Model model, CalculatorForm form, BindingResult binding) {
+        if(binding.hasErrors()) {
+            return "add"; // powrót do formularza
+        }
+        int result = calculatorService.add(form.getX(), form.getY());
         
         form.setOperation("+");
         model.addAttribute("result", result);
         return "result";
     }
 
-    @RequestMapping("/multiply")
-    public String multiplyNumbers(Model model, CalculatorForm form) {
-        int result = form.getX() * form.getY();
+    @PostMapping("/multiply")
+    public String multiplyNumbers(Model model, CalculatorForm form, BindingResult binding) {
+        if(binding.hasErrors()) {
+            return "multiply"; // powrót do formularza
+        }
+        int result = calculatorService.multiply(form.getX(), form.getY());
         
         form.setOperation("*");
         model.addAttribute("result", result);
         return "result";
     }
 
-    @RequestMapping("/calculate")
-    public String doCalculations(Model model, CalculatorForm form) {
+    @PostMapping("/calculate")
+    public String doCalculations(Model model, CalculatorForm form, BindingResult binding) {
+        if(binding.hasErrors()) {
+            return "calculate"; // powrót do formularza
+        }
         int result = 0;
         switch(form.getOperation()) {
             case "+":
-                result = form.getX() + form.getY();
+                result = calculatorService.add(form.getX(), form.getY());
                 break;
             case "-":
-                result = form.getX() - form.getY();
+                result = calculatorService.subtract(form.getX(), form.getY());
                 break;
             case "*":
-                result = form.getX() * form.getY();
+                result = calculatorService.multiply(form.getX(), form.getY());
                 break;
         }
         
@@ -49,4 +64,22 @@ public class CalculatorController {
         
         return "result";
     }
+    @GetMapping("/add")
+    public String addForm(CalculatorForm form) 
+    {
+        return "add";
+    }
+
+    @GetMapping("/multiply")
+    public String multiplyForm(CalculatorForm form) 
+    {
+        return "multiply";
+    }
+
+    @GetMapping("/calculate")
+    public String calculateForm(CalculatorForm form)
+    {
+        return "calculate";
+    }
+
 }
